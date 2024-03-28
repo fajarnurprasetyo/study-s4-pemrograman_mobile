168cf44bf4df222d050b6556b29f84561251b0ba
Fajar Nur Prasetyo <fajarnurprasetyo.2@gmail.com>
2024-03-28 19:30:56 +0700

    Tugas 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,14 +32,17 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
-
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("io.noties.markwon:core:4.6.2")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
+
--- app/src/main/java/in/blackant/helloworld/MainActivity.java
+++ app/src/main/java/in/blackant/helloworld/MainActivity.java
@@ -1,24 +1,92 @@
 package in.blackant.helloworld;
 
+import android.content.pm.PackageInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import in.blackant.helloworld.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mBinding;
+    private MessageAdapter mAdapter;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        mAdapter = new MessageAdapter(this);
+        mQueue = Volley.newRequestQueue(this);
+
+        setSupportActionBar(mBinding.toolbar);
+
+        mBinding.messageContainer.setLayoutManager(new LinearLayoutManager(this));
+        mBinding.messageContainer.setAdapter(mAdapter);
+
+        mBinding.sendButton.setOnClickListener((v) -> {
+            final Editable message = mBinding.messageInput.getText();
+            if (message.length() > 0) {
+                mBinding.sendButton.setEnabled(false);
+                final String text = message.toString();
+                final SimiRequest req = new SimiRequest(this, text, res -> {
+                    try {
+                        if (res.getInt("status") == 200) {
+                            mAdapter.addMessage(res.getString("atext"), false);
+                        }
+                    } catch (JSONException err) {
+                        Log.e("SIMI", err.toString());
+                    }
+                    mBinding.sendButton.setEnabled(true);
+                }, err -> {
+                    Log.e("SIMI", err.toString());
+                    mBinding.sendButton.setEnabled(true);
+                });
+                mQueue.add(req);
+                mAdapter.addMessage(text, true);
+                message.clear();
+            }
         });
+
+        setContentView(mBinding.getRoot());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_info) {
+//            TODO: Open about dialog
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
