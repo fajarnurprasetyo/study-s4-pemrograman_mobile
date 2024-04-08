a8622b67a31676b20de6c11e4171af4825e5403a
Fajar Nur Prasetyo <fajarnurprasetyo.2@gmail.com>
2024-04-09 06:00:47 +0700

    close #7

--- app/src/main/java/in/blackant/helloworld/MainActivity.java
+++ app/src/main/java/in/blackant/helloworld/MainActivity.java
@@ -1,15 +1,18 @@
 package in.blackant.helloworld;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import in.blackant.helloworld.databinding.ActivityMainBinding;
 
@@ -29,17 +32,51 @@ public class MainActivity extends AppCompatActivity {
         final ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setSupportActionBar(binding.toolbar);
 
-        final View.OnClickListener listener = (v) -> {
-            final CheckBox cb = (CheckBox) v;
-            if (cb.getId() == R.id.cb1) {
-                binding.tv1.setText(cb.isChecked() ? "12.000" : "0");
-            } else if (cb.getId() == R.id.cb2) {
-                binding.tv2.setText(cb.isChecked() ? "8.000" : "0");
+        @SuppressLint({"DefaultLocale", "SetTextI18n"}) final View.OnClickListener listener = (v) -> {
+            final Button btn = (Button) v;
+            if (btn.getId() != R.id.btn_eq) {
+                binding.op.setText(btn.getText());
+                return;
             }
+
+            if (binding.n1.getText() == null || binding.n1.getText().length() == 0 || binding.n2.getText() == null || binding.n2.getText().length() == 0) {
+                Snackbar.make(binding.getRoot(), "Kolom tidak boleh kosong!", Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+
+            final double n1 = Double.parseDouble(binding.n1.getText().toString());
+            final double n2 = Double.parseDouble(binding.n2.getText().toString());
+            double result;
+            switch (binding.op.getText().toString()) {
+                case "-":
+                    result = n1 - n2;
+                    break;
+                case "+":
+                    result = n1 + n2;
+                    break;
+                case "÷":
+                    if (n2 == 0) {
+                        Snackbar.make(binding.getRoot(), "Tidak bisa membagi dengan 0!", Snackbar.LENGTH_SHORT).show();
+                        return;
+                    }
+                    result = n1 / n2;
+                    break;
+                case "×":
+                    result = n1 * n2;
+                    break;
+                default:
+                    Snackbar.make(binding.getRoot(), "Harap pilih operan terlebih dahulu!", Snackbar.LENGTH_SHORT).show();
+                    return;
+            }
+            if ((int) result != result) binding.result.setText(String.format("%.2f", result));
+            else binding.result.setText(Integer.toString((int) result));
         };
 
-        binding.cb1.setOnClickListener(listener);
-        binding.cb2.setOnClickListener(listener);
+        binding.btnMin.setOnClickListener(listener);
+        binding.btnAdd.setOnClickListener(listener);
+        binding.btnDiv.setOnClickListener(listener);
+        binding.btnTim.setOnClickListener(listener);
+        binding.btnEq.setOnClickListener(listener);
 
         setContentView(binding.getRoot());
     }
